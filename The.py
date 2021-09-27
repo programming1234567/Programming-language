563dafb6e1378b00ca5794fcc900ac61790341b0
programming1234567 <90536647+programming1234567@users.noreply.github.com>
2021-09-27 17:56:22 -0500

    Add files via upload

---
+++ The.py
@@ -0,0 +1,100 @@
+output = float(0)
+##########################################
+# + to add 1 to the current output
+# - to subtract 1 from the current output
+# * to multiply the current output by 2
+# / to divide the current output by 2
+##########################################
+# inputs
+a = input("")
+b = input("")
+c = input("")
+d = input("")
+e = input("")
+f = input("")
+g = input("")
+h = input("")
+i = input("")
+j = input("")
+# if statments
+if a == "+":
+    output += 1
+if a == "-":
+    output -= 1
+if a == "*":
+    output *= 2
+if a == "/":
+    output /= 2
+if b == "+":
+    output += 1
+if b == "-":
+    output -= 1
+if b == "*":
+    output *= 2
+if b == "/":
+    output /= 2
+if c == "+":
+    output += 1
+if c == "-":
+    output -= 1
+if c == "*":
+    output *= 2
+if c == "/":
+    output /= 2
+if d == "+":
+    output += 1
+if d == "-":
+    output -= 1
+if d == "*":
+    output *= 2
+if d == "/":
+    output /= 2
+if e == "+":
+    output += 1
+if e == "-":
+    output -= 1
+if e == "*":
+    output *= 2
+if e == "/":
+    output /= 2
+if f == "+":
+    output += 1
+if f == "-":
+    output -= 1
+if f == "*":
+    output *= 2
+if f == "/":
+    output /= 2
+if g == "+":
+    output += 1
+if g == "-":
+    output -= 1
+if g == "*":
+    output *= 2
+if g == "/":
+    output /= 2
+if h == "+":
+    output += 1
+if h == "-":
+    output -= 1
+if h == "*":
+    output *= 2
+if h == "/":
+    output /= 2
+if i == "+":
+    output += 1
+if i == "-":
+    output -= 1
+if i == "*":
+    output *= 2
+if i == "/":
+    output /= 2
+if j == "+":
+    output += 1
+if j == "-":
+    output -= 1
+if j == "*":
+    output *= 2
+if j == "/":
+    output /= 2
+print(output)
